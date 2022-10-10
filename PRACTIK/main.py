802a681d45145278cc3d655baef3fa7fd50a2cfc
Vedagorich <yas.taras@mail.ru>
2022-10-10 10:23:36 +0300

    тестовый первый комит

---
+++ PRACTIK/main.py
@@ -0,0 +1,12 @@
+import requests
+from datetime import datetime
+
+def get_data():
+    req = requests.get("https://yobit.net/api/3/ticker/btc_usd")
+    response = req.json()
+    #print(response)
+    sell_price = response["btc_usd"]["sell"]
+    print(f"{datetime.now().strftime('%Y-%m-%d')}\nSell BTC price: {sell_price}" )
+
+if __name__ == "__main__":
+    get_data()
